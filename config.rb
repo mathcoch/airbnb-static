392781cac7ab35c6b6b15aa01ec53973f9a6ff5b
mathcoch <mathieu@cochet.me>
2017-05-08 15:17:42 +0200

    With profile

--- config.rb
+++ config.rb
@@ -90,6 +90,6 @@ activate :deploy do |deploy|
   # deploy.commit_message = 'custom-message' # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
 end
 
-["seb", "romain", "chirac", "banane", "andré", "pierrot", "lefou", "pomme"].each do |name|
+["seb", "romain", "chirac", "youpi", "andré", "pierrot", "lefou", "pomme"].each do |name|
   proxy "/flats/#{name}.html", "/flats/show.html", :locals => { :owner => name }, :ignore => true
 end
